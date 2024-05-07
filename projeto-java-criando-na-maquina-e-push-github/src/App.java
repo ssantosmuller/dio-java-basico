e324de020e71fd25ea3666d701a163426c042bfa
ssantosmuller <ssantos.muller@gmail.com>
2024-05-07 11:59:47 -0300

    subindo para github

--- projeto-java-criando-na-maquina-e-push-github/src/App.java
+++ projeto-java-criando-na-maquina-e-push-github/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Neste trabalho crio um projeto na maquina e subo ele para o GitHub");
+        System.out.println("Neste trabalho crio um projeto na maquina e subo ele para o GitHub teste");
     }
 }
